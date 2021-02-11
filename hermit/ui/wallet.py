593dbe5ef3818e8e06d6ead9d4007a16947ba3d7
Michael Flaxman <mflaxman@gmail.com>
2021-02-11 12:09:39 -0600

    stashing sort of working PSBT version

--- hermit/ui/wallet.py
+++ hermit/ui/wallet.py
@@ -38,7 +38,7 @@ def echo():
 
 
 @wallet_command("sign-bitcoin")
-def sign_bitcoin(psbt_b64):
+def sign_bitcoin(unsigned_psbt_b64):
     """usage:  sign-bitcoin
 
     Create a signature for a Bitcoin transaction.
@@ -56,7 +56,7 @@ def sign_bitcoin(psbt_b64):
     Creating a signature requires unlocking the wallet.
 
     """
-    BitcoinSigner(state.Wallet, state.Session, psbt_b64=psbt_b64).sign(testnet=state.Testnet)
+    BitcoinSigner(state.Wallet, state.Session, unsigned_psbt_b64=unsigned_psbt_b64).sign(testnet=state.Testnet)
 
 
 @wallet_command("export-xpub")
--- hermit/wallet.py
+++ hermit/wallet.py
@@ -1,50 +1,8 @@
-from re import match
-from typing import Tuple
-
 from buidl import HDPrivateKey, HDPublicKey
 from mnemonic import Mnemonic
 
 from hermit import shards
 from hermit.errors import HermitError
-
-
-def compressed_private_key_from_bip32(bip32_xkey: str) -> bytes:
-    """Return a compressed private key from the given BIP32 path"""
-    bip32_args = bip32_deserialize(bip32_xkey)
-    # cut off 'compressed' byte flag (only for private key!)
-    return bip32_args[5][:-1]
-
-
-def compressed_public_key_from_bip32(bip32_xkey: str) -> bytes:
-    """Return a compressed public key from the given BIP32 path"""
-    bip32_args = bip32_deserialize(bip32_xkey)
-    return bip32_args[5]
-
-
-def _hardened(id: int) -> int:
-    hardening_offset = 2 ** 31
-    return hardening_offset + id
-
-
-def _decode_segment(segment: str) -> int:
-    if segment.endswith("'") or segment.lower().endswith("h"):
-        return _hardened(int(segment[:-1]))
-    else:
-        return int(segment)
-
-
-def bip32_sequence(bip32_path: str) -> Tuple[int, ...]:
-    """Turn a BIP32 path into a tuple of deriviation points"""
-    bip32_path_regex = "^m(/[0-9]+'?)+$"
-
-    if not match(bip32_path_regex, bip32_path):
-        raise HermitError("Not a valid BIP32 path.")
-
-    return tuple(
-        _decode_segment(segment)
-        for segment in bip32_path[2:].split("/")
-        if len(segment) != 0
-    )
 
 
 class HDWallet(object):
@@ -55,17 +13,17 @@ class HDWallet(object):
     """
 
     def __init__(self, testnet=False) -> None:
-        self.root_priv = None
+        self.root_xpriv = None
         self.xfp_hex = None  # root fingerprint in hex
         self.testnet = testnet
         self.shards = shards.ShardSet()
         self.language = "english"
 
     def unlocked(self) -> bool:
-        return self.root_priv is not None
+        return self.root_xpriv is not None
 
     def unlock(self, passphrase: str = "") -> None:
-        if self.root_priv is not None:
+        if self.root_xpriv is not None:
             return
 
         mnemonic = Mnemonic(self.language)
@@ -75,28 +33,33 @@ class HDWallet(object):
         if mnemonic.check(words):
             seed = Mnemonic.to_seed(words, passphrase=passphrase)
             hd_obj = HDPrivateKey.from_seed(seed, testnet=self.testnet)
-            self.root_priv = hd_obj.xprv()
-            self.xfp_hex = hd_obj.fingerprint().hex()  # this will be later needed to identify us as a signer
+            self.root_xpriv = hd_obj.xprv()
+            self.xfp_hex = hd_obj.fingerprint().hex()  # later needed to identify us as cosigner
         else:
             raise HermitError("Wallet words failed checksum.")
 
 
     def lock(self) -> None:
-        self.root_priv = None
+        self.root_xpriv = None
 
     def extended_public_key(self, bip32_path: str) -> str:
         self.unlock()
         xprv = self.extended_private_key(bip32_path)
-        # FIXME: allow passing in of SLIP132 version byte here? Going with default xpub for now
+        # TODO: do we want to allow passing in SLIP132 version byte (ypub/zpub)?
+        # Going with default xpub ONLY for now
         return HDPrivateKey.parse(xprv).xpub()
-
-    def public_key(self, bip32_path: str) -> str:
-        # TODO: does this have a use-case or should it be tossed?
-        xpub = self.extended_public_key(bip32_path)
-        return HDPublicKey.parse(xpub).hash160().hex()
 
     def extended_private_key(self, bip32_path: str) -> str:
         self.unlock()
-        xprv = self.root_priv
+        xprv = self.root_xpriv
         hd_obj = HDPrivateKey.parse(xprv)
         return hd_obj.traverse(path=bip32_path).xprv()
+
+    def get_child_private_key_objs(self, bip32_paths):
+        """
+        Derive child private key objects and return them to (co)sign a transaction.
+        """
+        hd_priv_obj = HDPrivateKey.parse(self.root_xpriv)
+        return [
+            hd_priv_obj.traverse(bip32_path).private_key for bip32_path in bip32_paths
+        ]
